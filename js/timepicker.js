24844bd957b87b6ed5e416be113787e9205c3617
mistic10 <hypnolog@yahoo.fr>
2017-07-10 20:05:02 +0200

    création et positionnement du picker

--- js/timepicker.js
+++ js/timepicker.js
@@ -2,6 +2,69 @@
 	$.fn.timepicker = function(){
 		//initialisation des éléments
 		this.each(function(i){
+			$(this)
+			.val('00:00')
+			.attr({
+				'maxlength' : 5,
+				'data-picker' : i
+				})
+			.click(function(){
+				$('#ui-timepicker-' + i).show();
+				});
+			
+			createPicker($(this), i);
 			});
+		
+		//création du picker
+		function createPicker(elt, i){
+			$('body').append(
+				$(document.createElement('div'))
+				.attr({
+					id : 'ui-timepicker-' + i,
+					class : 'ui-timepicker'
+					})
+				.append([
+					//selection de l'heure
+					numField('h', 24),
+					
+					//séparation
+					$(document.createElement('span'))
+					.text(':'),
+					
+					//selection des minutes
+					numField('m', 60),
+					
+					//boutton de fermeture du picker
+					$(document.createElement('button'))
+					.click(function(){
+						$('#ui-timepicker-' + i).hide();
+						})
+					])
+				.css({
+					position : 'absolute',
+					left : elt.offset().left,
+					top : elt.offset().top + elt.outerHeight()
+					})
+				.hide()
+				);
+			}
+		
+		//céation des select du picker
+		function numField(selType, maxNum){
+			var optGroupElt = $(document.createElement('optgroup'));
+			
+			var numElt = $(document.createElement('select'))
+			.addClass('picker-' + selType)
+			.append(optGroupElt);
+			
+			for(var i = 0; i <= maxNum; i ++){
+				optGroupElt.append(
+					$(document.createElement('option'))
+					.text(i)
+					);
+				}
+			
+			return numElt;
+			}
 		}
 	})(jQuery)
