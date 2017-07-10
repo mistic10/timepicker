2221df9dc1b80c763be09fb8e74270eb8655db62
mistic10 <hypnolog@yahoo.fr>
2017-07-10 20:31:58 +0200

    ajout des propriétés, arguments, et valeure par défauts

--- js/timepicker.js
+++ js/timepicker.js
@@ -1,9 +1,36 @@
 (function($){
-	$.fn.timepicker = function(){
+	$.fn.timepicker = function(args){
+		//définition des propriétés
+		var prop = {
+			set separator(val){
+				this.sep = (/^:|h/i.test(val)) ? val : ':';
+				},
+			set model(val){
+				this.mod = (val == 12 || val == 24) ? val : 24;
+				},
+			set class(val){
+				this.cla = (val.split(' ').length) ? val : 'ui-timepicker';
+				},
+			
+			get separator(){
+				return (typeof this.sep != 'undefined') ? this.sep : ':';
+				},
+			get model(){
+				return (typeof this.mod != 'undefined') ? this.mod : 24;
+				},
+			get class(){
+				return (typeof this.cla != 'undefined') ? this.cla : 'ui-timepicker';
+				}
+			};
+		
+		//assignation des arguments aux propriétés
+		if(typeof args != 'undefined')
+			$.extend(prop, args);
+		
 		//initialisation des éléments
 		this.each(function(i){
 			$(this)
-			.val('00:00')
+			.val('00' + prop.separator + '00')
 			.attr({
 				'maxlength' : 5,
 				'data-picker' : i
@@ -17,29 +44,47 @@
 		
 		//création du picker
 		function createPicker(elt, i){
+			var eltsPicker = [
+					//selection de l'heure
+					numField('h', prop.model),
+					
+					//séparation
+					$(document.createElement('span'))
+					.text(prop.separator),
+					
+					//selection des minutes
+					numField('m', 60)
+				];
+			
+			//selection de AM/PM
+			if(prop.model == 12){
+				eltsPicker.push(
+					$(document.createElement('select'))
+					.append([
+						$(document.createElement('option'))
+						.text('AM'),
+						$(document.createElement('option'))
+						.text('PM')
+						])
+					);
+				}
+			
+			//boutton de fermeture du picker
+			eltsPicker.push(
+				$(document.createElement('button'))
+				.click(function(){
+					$('#ui-timepicker-' + i).hide();
+					})
+				);
+			
+			//création et ajout du picker au body
 			$('body').append(
 				$(document.createElement('div'))
 				.attr({
 					id : 'ui-timepicker-' + i,
-					class : 'ui-timepicker'
+					class : prop.class
 					})
-				.append([
-					//selection de l'heure
-					numField('h', 24),
-					
-					//séparation
-					$(document.createElement('span'))
-					.text(':'),
-					
-					//selection des minutes
-					numField('m', 60),
-					
-					//boutton de fermeture du picker
-					$(document.createElement('button'))
-					.click(function(){
-						$('#ui-timepicker-' + i).hide();
-						})
-					])
+				.append(eltsPicker)
 				.css({
 					position : 'absolute',
 					left : elt.offset().left,
